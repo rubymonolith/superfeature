8c3044175a3ed53c5c26f7321f9312bf10b0c9aa
Brad Gessler <bradgessler@gmail.com>
2025-10-10 23:54:42 -0700

    Add specs for app/plans folder

--- lib/superfeature/engine.rb
+++ lib/superfeature/engine.rb
@@ -1,5 +1,11 @@
 module Superfeature
   class Engine < ::Rails::Engine
     isolate_namespace Superfeature
+
+    config.after_initialize do |app|
+      # Add app/plans to the application's autoload paths
+      plans_path = app.root.join("app/plans")
+      Rails.autoloaders.main.push_dir(plans_path) if plans_path.exist?
+    end
   end
 end
