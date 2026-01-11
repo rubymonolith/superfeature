ed379cdc459ca1a435ba035c7ad088e1585cd59b
Brad Gessler <bradgessler@gmail.com>
2026-01-10 22:18:14 -0800

    Added `require "superfeature"` at the top of `lib/superfeature/core_ext.rb` so that `require "superfeature/core_ext"` works standalone without getting "uninitialized constant" errors.

--- lib/superfeature/core_ext.rb
+++ lib/superfeature/core_ext.rb
@@ -7,5 +7,6 @@
 #   10.to_price           # => Price(10)
 #   "49.99".to_price      # => Price(49.99)
 #
+require "superfeature"
 require "superfeature/core_ext/numeric"
 require "superfeature/core_ext/string"
