cb741cd4f98f49e4a24b50974c108c89418891b4
Brad Gessler <bradgessler@gmail.com>
2026-01-11 04:42:46 -0800

    Fix enable feature bug

--- lib/superfeature/plan.rb
+++ lib/superfeature/plan.rb
@@ -39,8 +39,8 @@ module Superfeature
       Feature.new(*, **, &)
     end
 
-    def enable(flag = true, *, **)
-      feature(*, **, limit: Limit::Boolean.new(enabled: flag))
+    def enable(*, **)
+      feature(*, **, limit: Limit::Boolean.new(enabled: true))
     end
 
     def disable(*, **)
